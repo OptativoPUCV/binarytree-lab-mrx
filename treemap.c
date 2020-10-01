7407fefb28417779040bf5d91f8dfa7152e3a7b8
repl.it user <replituser@example.com>
2020-10-01 14:53:31 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -229,7 +229,7 @@ void * nextTreeMap(TreeMap * tree)
   {
     TreeNode* aux = tree->current;
 
-    while(tree->current->parent != NULL && aux->key < tree->current->key)
+    while(tree->current->parent != NULL && aux->key <= tree->current->key)
       tree->current = tree->current->parent;
     
     if(tree->current == tree->root && tree->current->key != aux->key)
