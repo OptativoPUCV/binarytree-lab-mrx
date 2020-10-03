689d7e8467599d1d9ea5f698a84d30f8ace69fcc
repl.it user <replituser@example.com>
2020-10-03 18:18:57 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -176,7 +176,7 @@ void * upperBound(TreeMap * tree, void* key)
   if(tree->root == NULL || tree == NULL) return NULL;
 
   TreeNode* aux_node = tree->root;
-  TreeNode* higher = tree->root;
+  TreeNode* higher = NULL;
 
   while(aux_node != NULL)
   {
