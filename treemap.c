4360e3c05950b902eb6aa7f4a72861f327673100
repl.it user <replituser@example.com>
2020-10-03 19:26:14 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -186,7 +186,7 @@ void * upperBound(TreeMap * tree, void* key)
     {
       higher = aux_node;
       aux_node = aux_node->left;
-    }else if(aux_node->key != key) return aux_node->value;
+    }else if(aux_node->key == key) return aux_node->value;
     else return NULL;
   }
   
